2d656382113e00722aaf639f0c8a48ce5919262d
Guilherme Dudu Berti <guiberti1227@gmail.com>
2025-08-31 15:56:44 -0300

    atv finalizada - revisada

--- Aula27.08/SistemaGerenciamentoBliblioteca/LibraryManagementSystem.java
+++ Aula27.08/SistemaGerenciamentoBliblioteca/LibraryManagementSystem.java
@@ -2,20 +2,17 @@ package SistemaGerenciamentoBliblioteca;
 
 public class LibraryManagementSystem {
     public static void main(String[] args) {
-        // Criar instâncias dos materiais
         LibraryItem book = new Book("1984", "George Orwell", 1949, "Distopia");
         LibraryItem magazine = new Magazine("National Geographic", "Vários Autores", 2023, 150);
         LibraryItem digitalMedia = new DigitalMedia("Inception", "Christopher Nolan", 2010, "MP4");
-        // Array para demonstrar polimorfismo
         LibraryItem[] items = {book, magazine, digitalMedia};
         for (LibraryItem item : items) {
             System.out.println("-----");
             item.displayInfo();
             item.borrow();
-            item.borrow(); // tentar emprestar novamente para testar
+            item.borrow();
             item.returnItem();
-            item.returnItem(); // tentar devolver novamente para testar
-            // Comportamentos específicos via instanceof e cast
+            item.returnItem();
             if (item instanceof Book) {
                 ((Book) item).readSample();
             } else if (item instanceof Magazine) {
